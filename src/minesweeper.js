dc7025076b3bb4bd5de4829d12548db180b3ba99
jsand3 <sanders3.j@gmail.com>
2018-05-02 22:31:39 -0500

    Part II Updated

--- src/minesweeper.js
+++ src/minesweeper.js
@@ -1,6 +1,8 @@
 const printBoard = board => {
   console.log("Current Board:");
-  //console.log(board[0][0]);
+  console.log(board[0].join(' | ')); // row 1
+  console.log(board[1].join(' | ')); // row 2
+  console.log(board[2].join(' | ')); // row 3
 };
 let board = [
   ['','',''],
@@ -9,12 +11,6 @@ let board = [
 ];
 
 printBoard(board);
-console.log(board[0].join(' | ')); // row 1
-console.log(board[1].join(' | ')); // row 2
-console.log(board[2].join(' | ')); // row 3
 board[0][1] = 1;
 board[2][2] = "B";
 printBoard(board);
-console.log(board[0].join(' | ')); // row 1
-console.log(board[1].join(' | ')); // row 2
-console.log(board[2].join(' | ')); // row 3
