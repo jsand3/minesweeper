4a5250c0cafe08db905496c8cffda18c966d81f7
jsand3 <sanders3.j@gmail.com>
2018-05-02 22:18:26 -0500

    Part II Completed

--- src/minesweeper.js
+++ src/minesweeper.js
@@ -1,5 +1,20 @@
+const printBoard = board => {
+  console.log("Current Board:");
+  //console.log(board[0][0]);
+};
 let board = [
-  [],
-  [],
-  []
+  ['','',''],
+  ['','',''],
+  ['','','']
 ];
+
+printBoard(board);
+console.log(board[0].join(' | ')); // row 1
+console.log(board[1].join(' | ')); // row 2
+console.log(board[2].join(' | ')); // row 3
+board[0][1] = 1;
+board[2][2] = "B";
+printBoard(board);
+console.log(board[0].join(' | ')); // row 1
+console.log(board[1].join(' | ')); // row 2
+console.log(board[2].join(' | ')); // row 3
