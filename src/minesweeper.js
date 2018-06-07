654983e1a223e2938669ce9b805cd595ba05d306
jsand3 <sanders3.j@gmail.com>
2018-06-06 22:11:22 -0500

    Start of Minesweeper III

--- src/minesweeper.js
+++ src/minesweeper.js
@@ -1,16 +1,13 @@
+const generatePlayerBoard = (numberOfRows,numberOfColumns) => {
+
+};
+
+
+
+
 const printBoard = board => {
   console.log("Current Board:");
   console.log(board[0].join(' | ')); // row 1
   console.log(board[1].join(' | ')); // row 2
   console.log(board[2].join(' | ')); // row 3
 };
-let board = [
-  ['','',''],
-  ['','',''],
-  ['','','']
-];
-
-printBoard(board);
-board[0][1] = 1;
-board[2][2] = "B";
-printBoard(board);
